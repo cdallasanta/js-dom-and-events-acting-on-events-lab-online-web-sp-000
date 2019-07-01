e03df350bea6a34b13784523e556373d01edfed6
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-01 21:00:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function addNewLiOnClick() {
 }
 
 function clearEmployeeListOnLinkClick() {
-  $("ul.employee-list").empty();
+  $("ul.employee-list li").remove();
 }
 
 // addNewLiOnClick(): This adds the employee name as an li element on click and clears the input value.
