f280a092953581bd6d64582591246d08e375b0bd
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-01 20:48:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function retrieveEmployeeInformation() {
 
 }
 
-: This retrieves the employee information from the input. It returns a string equal to the value in the input.
-addNewElementAsLi(): This function takes the value retrieved from the previous retrieveEmployeeInformation function and adds that string to a new list element which is appended to the ul with class "employee-list".
-addNewLiOnClick(): This adds the employee name as an li element on click and clears the input value.
-clearEmployeeListOnLinkClick(): This empties out the employee list when "Clear employee list" button clicked.
+// : This retrieves the employee information from the input. It returns a string equal to the value in the input.
+// addNewElementAsLi(): This function takes the value retrieved from the previous retrieveEmployeeInformation function and adds that string to a new list element which is appended to the ul with class "employee-list".
+// addNewLiOnClick(): This adds the employee name as an li element on click and clears the input value.
+// clearEmployeeListOnLinkClick(): This empties out the employee list when "Clear employee list" button clicked.
