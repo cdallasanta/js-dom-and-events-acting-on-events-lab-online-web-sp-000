7f9e709bc864f52c508f41c467ac03d8f291d157
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-01 20:48:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function preventRefreshOnSubmit(){
-    document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+  document.querySelector('form').addEventListener('submit', function(event){
+    event.preventDefault()
+  })
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+
+}
+
+: This retrieves the employee information from the input. It returns a string equal to the value in the input.
+addNewElementAsLi(): This function takes the value retrieved from the previous retrieveEmployeeInformation function and adds that string to a new list element which is appended to the ul with class "employee-list".
+addNewLiOnClick(): This adds the employee name as an li element on click and clears the input value.
+clearEmployeeListOnLinkClick(): This empties out the employee list when "Clear employee list" button clicked.
